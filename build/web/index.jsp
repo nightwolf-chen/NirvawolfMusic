cf8fbb3d229bb7138701313513e44bb1fc678b68
Bruce Chen <466202783@qq.com>
2014-02-19 15:51:08 +0800

    成功连上appfog数据库
    之前一直以为是appfog使用的mysqldriver和我使用的mysqldriver不一致导致的问题。后来才发现是自己的代码出现的问题！

--- build/web/index.jsp
+++ build/web/index.jsp
@@ -4,6 +4,7 @@
     Author     : bruce
 --%>
 
+<%@page import="java.sql.DriverManager"%>
 <%@page import="javax.sql.RowSet"%>
 <%@page import="org.json.JSONArray"%>
 <%@page import="org.json.JSONObject"%>
@@ -22,46 +23,23 @@
     <body>
         <%
             String databaseInfo = java.lang.System.getenv("VCAP_SERVICES");
-            out.print(databaseInfo + "<br>");
             ConnectionManager conMgr = new ConnectionManager();
-            out.println(conMgr.getConnectionStr());
             Connection con = conMgr.getConnection();
-            if (con == null) {
-                out.println("Connection failed!");
-            } else {
-                out.println("Connected!");
+            
+            Statement stmt = EasyDatabaseAccessor.createStatement(con);
+            ResultSet rs = EasyDatabaseAccessor.select(stmt, "select count(songid) from song");
+            
+            while(rs.next()){
+                out.println("Database connected!");
+                break;
             }
-
-            if (databaseInfo != null) {
-                JSONObject jsonObject = new JSONObject(databaseInfo);
-                JSONArray mysqlServices = jsonObject.getJSONArray("mysql-5.1");
-                JSONObject mysqlObj = mysqlServices.getJSONObject(0);
-                JSONObject credentials = mysqlObj.getJSONObject("credentials");
-
-                String ServerHost = credentials.getString("host");
-                String port = String.valueOf(credentials.getInt("port"));
-                String DatabaseName = credentials.getString("name");
-                String userName = credentials.getString("username");
-                String passWord = credentials.getString("password");
-
-                out.print("<br>" + userName + "<br>");
-                out.print("<br>" + passWord + "<br>");
-            }
-//            Statement stmt = EasyDatabaseAccessor.createStatement(con);
-//            ResultSet rs = EasyDatabaseAccessor.select(stmt, "select count(singerid) from singer");
-//            
-//            while(rs.next()){
-//                out.println("Database connected!");
-//                break;
-//            }
-//            
-//            rs.close();
-//            rs = null;
-//            stmt.close();
-//            stmt = null;
-//            con.close();
-//            con = null;
-
+            
+            rs.close();
+            rs = null;
+            stmt.close();
+            stmt = null;
+            con.close();
+            con = null;
 
         %>
     </body>
--- src/java/db/ConnectionManager.java
+++ src/java/db/ConnectionManager.java
@@ -13,7 +13,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 /**
  *
  * @author nightwolf 陈纪栋
@@ -25,66 +24,75 @@ public class ConnectionManager {
     private final String Encode = "utf8";
     private String ServerHost = "localhost";
     private String port = "3306";
-    private String conStr = "jdbc:mysql://" + ServerHost + ":" + port + "/" + DatabaseName + "?characterEncoding=" + Encode;
     private String userName = "root";
     private String passWord = "123";
-    
-/*
-    {"mysql-5.1":[
-    {
-        "name":"mysql-4f700",
-        "label":"mysql-5.1",
-        "plan":"free",
-        "tags":["mysql","mysql-5.1","relational"],
-        "credentials":{
-            "name":"d6d665aa69817406d8901cd145e05e3c6",
-            "hostname":"mysql-node01.us-east-1.aws.af.cm",
-            "host":"mysql-node01.us-east-1.aws.af.cm",
-            "port":3306,
-            "user":"uB7CoL4Hxv9Ny",
-            "username":"uB7CoL4Hxv9Ny",
-            "password":"pzAx0iaOp2yKB"
-        }
-    },
-    {
-        "name":"mysql-f1a13",
-        "label":"mysql-5.1",
-        "plan":"free",
-        "tags":["mysql","mysql-5.1","relational"],
-        "credentials":{
-            "name":"db777ab9da32047d99dd6cdae3aafebda",
-            "hostname":"mysql-node01.us-east-1.aws.af.cm",
-            "host":"mysql-node01.us-east-1.aws.af.cm",
-            "port":3306,
-            "user":"uJHApvZF6JBqT",
-            "username":"uJHApvZF6JBqT",
-            "password":"p146KmfkqGYmi"
-        }
-    }
-]}
-*/
-    
+
+    /*
+     {"mysql-5.1":[
+     {
+     "name":"mysql-4f700",
+     "label":"mysql-5.1",
+     "plan":"free",
+     "tags":["mysql","mysql-5.1","relational"],
+     "credentials":{
+     "name":"d6d665aa69817406d8901cd145e05e3c6",
+     "hostname":"mysql-node01.us-east-1.aws.af.cm",
+     "host":"mysql-node01.us-east-1.aws.af.cm",
+     "port":3306,
+     "user":"uB7CoL4Hxv9Ny",
+     "username":"uB7CoL4Hxv9Ny",
+     "password":"pzAx0iaOp2yKB"
+     }
+     },
+     {
+     "name":"mysql-f1a13",
+     "label":"mysql-5.1",
+     "plan":"free",
+     "tags":["mysql","mysql-5.1","relational"],
+     "credentials":{
+     "name":"db777ab9da32047d99dd6cdae3aafebda",
+     "hostname":"mysql-node01.us-east-1.aws.af.cm",
+     "host":"mysql-node01.us-east-1.aws.af.cm",
+     "port":3306,
+     "user":"uJHApvZF6JBqT",
+     "username":"uJHApvZF6JBqT",
+     "password":"p146KmfkqGYmi"
+     }
+     }
+     ]}
+     */
     public ConnectionManager() {
         //VCAP_SERVICES
         String databaseInfo = java.lang.System.getenv("VCAP_SERVICES");
-        
+
         if (databaseInfo != null) {
             try {
                 JSONObject jsonObject = new JSONObject(databaseInfo);
                 JSONObject mysqlObj = jsonObject.getJSONArray("mysql-5.1").getJSONObject(0);
                 JSONObject credentials = mysqlObj.getJSONObject("credentials");
-                
+
                 this.ServerHost = credentials.getString("hostname");
                 this.port = String.valueOf(credentials.getInt("port"));
                 this.DatabaseName = credentials.getString("name");
                 this.userName = credentials.getString("username");
                 this.passWord = credentials.getString("password");
-                
+
+//                this.ServerHost = "ap01-user01.c0ye1hvnkw6z.ap-southeast-1.rds.amazonaws.com";
+//                this.port = "3306";
+//                this.DatabaseName = "df610e5967901454283fef6c5512c5dab";
+//                this.userName = "u2ziJcRCIYKmR";
+//                this.passWord = "pFP2f91iXS1m5";
             } catch (JSONException ex) {
                 Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }else{
+
+                this.ServerHost = "localhost";
+                this.port = "10000";
+                this.DatabaseName = "df610e5967901454283fef6c5512c5dab";
+                this.userName = "uCgkiTgUzFbcz";
+                this.passWord = "p6v2zg4UBmR0C";
         }
-        
     }
 
     public Connection getConnection() {
@@ -95,7 +103,7 @@ public class ConnectionManager {
         }
         Connection con = null;
         try {
-            con = (Connection) DriverManager.getConnection(conStr, userName, passWord);
+            con = (Connection) DriverManager.getConnection(this.getConnectionStr(), this.userName, this.passWord);
         } catch (SQLException ex) {
             Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -103,7 +111,7 @@ public class ConnectionManager {
     }
 
     public String getConnectionStr() {
-        return "jdbc:mysql://" + ServerHost + ":3306/" + DatabaseName + "?CharacterEncoding=" + Encode;
+        return "jdbc:mysql://" + ServerHost + ":" + this.port + "/" + DatabaseName + "?CharacterEncoding=" + Encode;
     }
 
 }
